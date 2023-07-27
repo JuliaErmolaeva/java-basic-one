67b20dbeb4dcd9314846829dc8fbc4bf4feb43cf
Юлия Ермолаева <juliarmolaeva@mail.ru>
2023-07-27 10:01:05 +0300

    дз 5

--- src/main/java/ru/otus/java/basic/lesson5/Application.java
+++ src/main/java/ru/otus/java/basic/lesson5/Application.java
@@ -31,7 +31,7 @@ public class Application {
         int sum = 0;
         for (int i = 0; i < data.length; i++) {
             if (data[i] > 5) {
-                sum = sum + data[i];
+                sum += data[i];
             }
         }
         System.out.println(sum);
@@ -44,18 +44,22 @@ public class Application {
 
     public static void increase(int number, int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = number + array[i];
+            array[i] += number;
         }
     }
 
     public static void sumLarger(int[] array) {
         int sum = 0;
         int sum2 = 0;
-        for (int i = 0; i < array.length / 2 + 1; i++) {
-            sum = sum + array[i];
+        int coef = 0;
+        if (array.length % 2 != 0) {
+            coef = 1;
+        }
+        for (int i = 0; i < array.length / 2 + coef; i++) {
+            sum += array[i];
         }
         for (int j = array.length / 2; j < array.length; j++) {
-            sum2 = sum2 + array[j];
+            sum2 += array[j];
         }
         System.out.println(Math.max(sum, sum2));
     }
